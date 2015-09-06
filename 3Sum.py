b9e91dd0a4d6b453d139c72114c2e449bf568a0b
Nicholas Mahlangu <cubemasternm@gmail.com>
2015-09-06 15:00:17 -0400

    3SumClosest.py

--- 3Sum.py
+++ 3Sum.py
@@ -1,7 +1,7 @@
 # Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique 
 # triplets in the array which gives the sum of zero.
 # Notes: 
-#   Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
+#   Elements in a triplet (a,b,c) must be in non-descending order. (ie, a leq b leq c)
 #   The solution set must not contain duplicate triplets.
 
 class Solution(object):
