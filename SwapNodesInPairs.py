88206684da133c57e569681c8799a860441df086
Nicholas Mahlangu <cubemasternm@gmail.com>
2015-09-09 02:31:51 -0400

    Added a comment

--- SwapNodesInPairs.py
+++ SwapNodesInPairs.py
@@ -4,7 +4,7 @@
 # Your algorithm should use only constant space. You may not modify the values 
 # in the list, only nodes itself can be changed.
 
-Definition for singly-linked list.
+# Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
         self.val = x
