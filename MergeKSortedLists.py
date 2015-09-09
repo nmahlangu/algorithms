d4d8c09a9d91309ed0c54fceb4c3ede8c20f97be
Nicholas Mahlangu <cubemasternm@gmail.com>
2015-09-09 00:51:37 -0400

    updated description of MergeKSortedLists.py

--- MergeKSortedLists.py
+++ MergeKSortedLists.py
@@ -28,6 +28,6 @@ class Solution(object):
         return head.next
 
 # Solution: Throw the head of each list into a Priority Queue. While the queue is not empty,
-# append that element to the tail of the new linked list, making sure to throw it's next element
-# into the priority queue when it is taken out. Time complexity is O(nlog(k)) and space complexity
-# is O(1) (since it's storing at most k nodes)
+# pop and append the next element to the tail of the new linked list, making sure to throw it's
+# next element into the priority queue when it is taken out. Time complexity is O(nlog(k)) and
+# space complexity is O(1) (since it's storing at most k nodes)
