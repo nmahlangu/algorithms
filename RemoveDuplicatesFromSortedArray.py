92bfa93dd80f4f2394215a15cb4330a8b8b7fad5
Nicholas Mahlangu <cubemasternm@gmail.com>
2015-09-10 01:41:49 -0400

    RemoveDuplicatesFromSortedArray.py

---
+++ RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,25 @@
+# Given a sorted array, remove the duplicates in place such that each element appear only once 
+# and return the new length.
+# Do not allocate extra space for another array, you must do this in place with constant memory.
+# For example,
+# Given input array nums = [1,1,2],
+# Your function should return length = 2, with the first two elements of nums being 1 and 2 
+# respectively. It doesn't matter what you leave beyond the new length.
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        nums.sort()
+        for i in xrange(len(nums)):
+            if i > 0:
+                while i < len(nums) and nums[i] == nums[i-1]:
+                    del nums[i]
+        return len(nums)    
+
+# Solution: Since the array is sorted, just delete all matching adjacent elements. Time complexity
+# is O(n) and space complexity is O(1).
