a46032246f2ffe8dcd452bb24f137b726c387b9d
Nicholas Mahlangu <cubemasternm@gmail.com>
2015-10-12 00:12:44 -0400

    Added a note

--- KthLargestElementInArray.py
+++ KthLargestElementInArray.py
@@ -1,4 +1,4 @@
-# Write a function that takes an array and returns the kth largest
+# Write a function that takes an array and returns the kth smallest
 # element in that array. Raise an exception if the inputs are invalid.
 
 from Queue import PriorityQueue
@@ -20,3 +20,7 @@ def get_kth_largest(arr,k):
 # dequeued the kth time. Time complexity is O(n + klog(n)), since you're
 # doing n O(1) inserts into the Priority Queue and then doing k get min
 # operations which each take O(log(n)), and space complexity is O(n).
+# 
+# Note: if this question asked for the kth largest element, just insert
+# the negated elements into the priority queue and then return the
+# negated kth element.
