6d486242dc259e864c0e6ae911b738d85e49fdf7
Nicholas Mahlangu <nicholas@nicholass-mbp.lan>
2026-02-12 17:55:57 -0800

    d

---
+++ v2/hellointerview/course_schedule.py
@@ -0,0 +1,60 @@
+"""
+You have to take a total of numCourses courses, which are labeled from 0 to numCourses - 1. 
+You are given a list of prerequisites pairs, where prerequisites[i] = [a, b] indicates that 
+you must complete course b before course a.
+
+Given the total number of courses and a list of prerequisite pairs, write a function to 
+determine if it is possible to finish all courses.
+
+numCourses = 3
+prerequisites = [[1, 0], [2, 1]]
+
+Output True
+
+        # build adjacency list
+        # calculate indegrees
+        # enqueue nodes with indegree 0
+        # continue until queue empty
+        # return processed == len input
+"""
+from typing import *
+from collections import defaultdict, deque
+import unittest
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]):
+        num_courses: int = numCourses
+
+        adj_list: dict[int, list[int]] = defaultdict(list)
+        indegree: list[int] = num_courses * [0]
+        for dest, src in prerequisites:
+            adj_list[src].append(dest)
+            indegree[dest] += 1
+
+        queue: deque[int] = deque([i for i in range(num_courses) if indegree[i] == 0])
+
+        order: list[int] = []
+        while queue:
+            curr: int = queue.popleft()
+            order.append(curr)
+
+            for neighbor in adj_list.get(curr, []):
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        return True if len(order) == num_courses else False
+
+
+class TestSolution(unittest.TestCase):
+    def test_example1(self):
+        actual = Solution().canFinish(3, [[1, 0], [2, 1]])
+        self.assertEqual(True, actual)
+    
+    def test_example2(self):
+        actual = Solution().canFinish(3, [[1, 0], [0, 1], [1,2]])
+        self.assertEqual(False, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ v2/hellointerview/word_search.py
@@ -0,0 +1,50 @@
+from typing import *
+
+"""
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are 
+horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+"""
+
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        if not board:
+            return False
+        elif not word:
+            return False
+
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if word[0] == board[row][col]:
+                    if self.backtrack(board, row, col, 0, word):
+                        return True
+
+        return False
+
+    def backtrack(self, board: List[List[str]], row: int, col: int, idx: int, word: str) -> bool:
+        if idx == len(word):
+            return True
+        elif not (0 <= row < len(board)):
+            return False
+        elif not (0 <= col < len(board[0])): 
+            return False
+        elif board[row][col] != word[idx]:
+            return False
+
+        old_let: str = board[row][col]
+        board[row][col] = "#"
+
+        found: bool = (
+            self.backtrack(board, row + 1, col, idx + 1, word) or
+            self.backtrack(board, row - 1, col, idx + 1, word) or
+            self.backtrack(board, row, col + 1, idx + 1, word) or
+            self.backtrack(board, row, col - 1, idx + 1, word)
+        )
+
+        board[row][col] = old_let
+        return found
+
+
